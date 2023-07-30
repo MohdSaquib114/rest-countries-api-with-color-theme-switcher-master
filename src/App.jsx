15645881d7352963002d9ba61424b42ec09ccf41
Mohd Saquib Mansoori <msmsaq114@gmail.com>
2023-07-30 13:27:01 +0530

    Update css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Header from './components/Header';
+import Input from './components/Input'
 
 export default function App() {
+  const [iseDark, setDark ] = useState(false);
+  useEffect(()=>{
+    document.documentElement.classList.toggle('dark-mode')
+  
+  },[iseDark])
   return (
-    <div>
-      
-    </div>
+    <section>
+      <Header iseDark={iseDark} setDark={setDark}/>
+      <Input />
+    </section>
   )
 }
