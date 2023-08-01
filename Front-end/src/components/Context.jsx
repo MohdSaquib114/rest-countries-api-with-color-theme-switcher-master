b8b3e20302b37d0cdb3c8ddbca473283f5e3f452
Mohd Saquib Mansoori <msmsaq114@gmail.com>
2023-08-01 23:02:37 +0530

    Add server

---
+++ Front-end/src/components/Context.jsx
@@ -0,0 +1,44 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+const ContextProvider = createContext();
+function Context({children}) {
+    const [countryData,setData] =useState([])
+    const [filter,setFilter] =useState('')
+    const [searchValue,setSearch] = useState('')
+    const [themeChanger, setTheme ] = useState(false);
+    
+    
+  
+    useEffect(()=>{
+        async function getCountryData(){
+          const res = await fetch("http://localhost:8000/v1/api/countries");
+          const data = await res.json();
+          setData(JSON.parse(data))
+ 
+        }
+        getCountryData()
+      },[])
+
+   
+  return <ContextProvider.Provider value={
+            {countryData,
+              setData,
+             filter,
+             setFilter,
+             searchValue,
+             setSearch,
+             themeChanger,
+             setTheme,
+            }
+        }>
+
+             {children}
+
+        </ContextProvider.Provider>
+}
+const useContextData = ()=>{
+    const data = useContext(ContextProvider)
+    if(data === undefined) throw new Error('Its used out side of context')
+    return data;
+}
+
+export {Context, useContextData}
